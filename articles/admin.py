0b01bd9e644a74218366bc2cedefb55ec8fb935c
Тимур Ахмедов <timons@MacBook-Pro-Timur.local>
2024-12-22 10:50:56 +0100

    Initial commit

---
+++ articles/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from .models import Article, Scope, Tag
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            if form.cleaned_data.get('is_main'):
+                count += 1
+            else:
+                continue
+        if count == 0:
+             raise ValidationError('Выберите основной раздел')
+        elif count > 1:
+            raise ValidationError('Основной раздел может быть только один')
+        return super().clean()
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
